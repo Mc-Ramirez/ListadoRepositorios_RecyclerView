dd92b27c20de9c55b00f84a615e38de954c4ffcf
Felipe Ramirez <73697209+Mc-Ramirez@users.noreply.github.com>
2021-11-09 11:32:52 +0100

    clase repositorio ok

---
+++ app/src/main/java/com/example/listadorepositorios_recyclerview/model/Repositorio.kt
@@ -0,0 +1,34 @@
+package com.example.listadorepositorios_recyclerview.model
+
+data class Repositorio(
+    val name:String,
+    val avartarURL : String,
+    val description : String,
+    val license: String?,
+    val topics: List<String>,
+    val language: String?
+){
+    constructor(respuestaRepositorio: Respuesta_Repositorios) : this(
+        respuestaRepositorio.name,
+        respuestaRepositorio.owner.avatarURL,
+        respuestaRepositorio.description,
+        respuestaRepositorio.license?.name,
+        respuestaRepositorio.topics,
+        respuestaRepositorio.language
+    )
+}
+
+fun Respuesta_Repositorios.aRepositorio():{
+    return Repositorio(
+        this.name,
+        this.owner.avatarURL,
+        this.description,
+        this.license?.name,
+        this.topics,
+        this.language
+    )
+}
+
+fun Array<Respuesta_Repositorios>.aListaRepositorio():List<Repositorio>{
+    return this.map { it.aRepositorio() }
+}
